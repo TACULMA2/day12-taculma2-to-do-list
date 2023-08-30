b3c823354f6d34d4888426c0fe552ed2041646f2
TACULMA2 <marigold.taculog@oocl.com>
2023-08-31 04:04:07 +0800

    feat: implement addTodo on ToDoGenerator component

--- src/components/ToDoGenerator.js
+++ src/components/ToDoGenerator.js
@@ -4,15 +4,17 @@ import { useDispatch } from "react-redux";
 import { addTodo } from './toDoReducer';
 import { v4 as uuidv4} from 'uuid';
 import '../ToDoGenerator.css';
+import { useTodo } from "../hooks/useToDo";
 
 
 const ToDoGenerator = () => {
+    const { addTodo } = useTodo();
     const dispatch = useDispatch();
     const [inputValue, setInputValue] = useState("");
     
-    const addTodoItem = () => {
+    const addTodoItem = async () => {
         if (inputValue.trim() !== "") {
-            dispatch(addTodo({text: inputValue, id: uuidv4(), done: false}));
+            await addTodo({text: inputValue, id: uuidv4(), done: false});
             setInputValue("");
         } else {
             message.error("Blank is not accepted. Please input a proper value.");
